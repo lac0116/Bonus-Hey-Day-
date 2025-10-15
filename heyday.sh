03240ca98b3b7b9ce68dfa9f81ed356a4173b65f
lac0116 <lac0116@auburn.edu>
2025-10-14 22:42:42 -0500

    Add files via upload
    Accidentally uploaded (old) bugged version, this is current version

--- heyday.sh
+++ heyday.sh
@@ -9,31 +9,30 @@ validName=0
 
 # Purpose of while loop: validates name is not empty
 # Since name is entered in the loop, validName variable must be updated to 1 to exit loop
-while [validName -eq 0]
+while [ $validName -eq 0 ] 
 do
-	read -p -n "Enter your name: " name # -p allows prompt to be printed before expecting input
-	# -n removes automatic newline after printed prompt
-	if [-z "$name"]; then
+	read -p "Enter your name: " name # -p allows prompt to be printed before expecting input
+	if [ -z "$name" ]; then
 		echo -e "No name entered. Please try again\n" # -e allows use of \n
 	else
 		validName=1
 	fi
 done
 
-echo "Hey $name! War Eagle!"
+echo -e "\nHey $name! War Eagle!"
 
 validCollege=0
 validSport=0
 
 # Purpose of while loop: validates college is not empty
 # Since college is entered in the loop, validCollege must be updated to 1 to exit loop
-while [$validCollege -eq 0] # -eq comparator '=='
+while [ $validCollege -eq 0 ] # -eq comparator '=='
 do
-	echo "What college are you in?"
-	echo "(Engineering/Business/Liberal Arts/Sciences/Agriculture/Education/Veterinary Medicine)"
+	echo -e "\nWhat college are you in?"
+	echo -e "(Engineering/Business/Liberal Arts/Sciences/Agriculture/Education/Veterinary Medicine)"
 	read college
-	if [-z "$college"]; then
-		echo -e "No college entered. Please try again\n"
+	if [ -z "$college" ]; then
+		echo -e "\nNo college entered. Please try again\n"
 	else
 		validCollege=1
 	fi
@@ -41,19 +40,19 @@ done
 
 # Purpose of while loop: validates sport is not empty
 # Since college is entered in the loop, validSport must be updated to 1 to exit loop
-while [$validSport -eq 0]
+while [ $validSport -eq 0 ]
 do
-	echo "What's your favorite Auburn sport?"
-	echo "(Football/Basketball/Baseball/Gymnastics/Other)"
+	echo -e "\nWhat's your favorite Auburn sport?"
+	echo -e "(Football/Basketball/Baseball/Gymnastics/Other)"
 	read sport
-	if [-z "$sport"]; then
-		echo -e "No sport entered. Please try again\n"
+	if [ -z "$sport" ]; then
+		echo -e "\nNo sport entered. Please try again\n"
 	else
 		validSport=1
 	fi
 done
 
-echo "---Your Auburn Connections---"
+echo -e "\n---Your Auburn Connections---"
 
 # --------Search Explanations------------
 # grep -i "$college" auburn_notables.txt: searches auburn_notables.txt for lines with instances of $college, -i removes case 
@@ -67,10 +66,10 @@ echo "---Your Auburn Connections---"
 notable=$(grep -i "$college" auburn_notables.txt | shuf -n 1 | awk -F'|' '{print $1 " - " $4}')
 
 # If notable is found, then it prints it. Otherwise prints error message.
-if [-n "$notable"]; then
-	echo -n "From Auburn $college: $notable"
+if [ -n "$notable" ]; then
+	echo -e "\nFrom Auburn $college: $notable"
 else
-	echo "Sorry, I could not find anyone from your college in my files right now. Make sure you are entering one of the listed options after the prompt!"
+	echo -e "\nSorry, I could not find anyone from your college in my files right now. Make sure you are entering one of the listed options after the prompt!\n"
 fi
 
 # --------Search Explanations------------
@@ -86,15 +85,20 @@ athlete=$(grep -i "$sport" auburn_notables.txt | shuf -n 1 | awk -F'|' '{print $
 
 # If athlete is found matching sport, then they are printed. Otherwise prints error message.
 
-if [-n "$athlete"]; then
-        echo -n "Auburn $sport legend: $athlete"
+if [ -n "$athlete" ]; then
+        echo -e "\nAuburn $sport legend: $athlete"
 else
-        echo "Sorry, I could not find anyone from this sport in my files right now. Make sure you are entering one of the listed options after the prompt!"
+        echo "\nSorry, I could not find anyone from this sport in my files right now. Make sure you are entering one of the listed options after the prompt!\n"
 fi
 
 # ----------Search Explanations--------------
-# awk 'BEGIN{RS=""; FS="\n"} {print $0}' auburn_facts.txt: awk organizes facts into lines because they are split up
-# Pipelined to shuf -n 1: selects one field to print
-echo "Auburn Fun Fact: $(awk 'BEGIN{RS=""; FS="\n"} {print $0}' auburn_facts.txt | shuf -n 1)"
+# awk '{printf "%s ", $0}' auburn_facts.txt: reads file and merges all lines into single line,
+#     replaces line breaks with spaces so wrapped lines become one continuous line
+# Pipelined to tr '.' '\n': replaces every period (.) with a newline, splits sentences that end w/periods into lines
+# Pipelined to tr '!' '\n': replaces every exclamation mark (!) with a newline, same purpose
+# Pipelined to sed '/^\s*$/d': deletes any empty lines
+# Pipelined to shuf -n 1: randomly selects one line from the list
+# Result directly printed
 
-echo "War Eagle, $name!"
+echo -e "\nAuburn Fun Fact: $(awk '{printf "%s ", $0}' auburn_facts.txt | tr '.' '\n' | tr '!' '\n' | sed '/^\s*$/d' | shuf -n 1)."
+echo -e "\nWar Eagle, $name!"
